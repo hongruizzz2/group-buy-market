69b163ce391c2b76176e218700490c2ecfa5206a
hongrui <1169948065@qq.com>
2025-03-14 16:53:38 +0800

    fix: 多线程异步数据加载 - 补充

--- group-buy-market-domain/src/main/java/com/hongrui/domain/activity/service/trial/node/MarketNode.java
+++ group-buy-market-domain/src/main/java/com/hongrui/domain/activity/service/trial/node/MarketNode.java
@@ -3,18 +3,19 @@ package com.hongrui.domain.activity.service.trial.node;
 import com.alibaba.fastjson.JSON;
 import com.hongrui.domain.activity.model.entity.MarketProductEntity;
 import com.hongrui.domain.activity.model.entity.TrialBalanceEntity;
+import com.hongrui.domain.activity.model.valobj.GroupBuyActivityDiscountVO;
+import com.hongrui.domain.activity.model.valobj.SkuVO;
 import com.hongrui.domain.activity.service.trial.AbstractGroupBuyMarketSupport;
 import com.hongrui.domain.activity.service.trial.factory.DefaultActivityStrategyFactory;
 import com.hongrui.domain.activity.service.trial.thread.QueryGroupBuyActivityDiscountVOThreadTask;
+import com.hongrui.domain.activity.service.trial.thread.QuerySkuVOFromDBThreadTask;
 import com.hongrui.types.design.framework.tree.StrategyHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import sun.nio.ch.ThreadPool;
 
 import javax.annotation.Resource;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeoutException;
+import java.util.concurrent.*;
 
 /**
  * @author hongrui
@@ -34,14 +35,23 @@ public class MarketNode extends AbstractGroupBuyMarketSupport<MarketProductEntit
     protected void multiThread(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws InterruptedException, TimeoutException, ExecutionException {
         // 异步查询活动配置
         QueryGroupBuyActivityDiscountVOThreadTask queryGroupBuyActivityDiscountVOThreadTask = new QueryGroupBuyActivityDiscountVOThreadTask(
-                requestParameter.getSource(),
-                requestParameter.getChannel(),
-                repository
+                    requestParameter.getSource(),
+                    requestParameter.getChannel(),
+                    repository
                 );
-        // 异步查询商品信息 - 在实际生产中，有同步库或商品查询接口 这里暂时使用DB方式查询
+        FutureTask<GroupBuyActivityDiscountVO> groupBuyActivityDiscountVOFutureTask = new FutureTask<>(queryGroupBuyActivityDiscountVOThreadTask);
+        threadPoolExecutor.execute(groupBuyActivityDiscountVOFutureTask);
 
+        // 异步查询商品信息 - 在实际生产中，有同步库或商品查询接口 这里暂时使用DB方式查询
+        QuerySkuVOFromDBThreadTask querySkuVOFromDBThreadTask = new QuerySkuVOFromDBThreadTask(requestParameter.getGoodsId(), repository);
+        FutureTask<SkuVO> skuVOFutureTask = new FutureTask<>(querySkuVOFromDBThreadTask);
+        threadPoolExecutor.execute(skuVOFutureTask);
 
         // 写入上下文
+        dynamicContext.setGroupBuyActivityDiscountVO(groupBuyActivityDiscountVOFutureTask.get(timeout, TimeUnit.MINUTES));
+        dynamicContext.setSkuVO(skuVOFutureTask.get(timeout, TimeUnit.MINUTES));
+
+        log.info("拼团商品查询试算服务-MarketNode userId:{} 异步线程加载数据「GroupBuyActivityDiscountVO、SkuVO」完成", requestParameter.getUserId());
     }
 
     @Override
@@ -56,6 +66,6 @@ public class MarketNode extends AbstractGroupBuyMarketSupport<MarketProductEntit
 
     @Override
     public StrategyHandler<MarketProductEntity, DefaultActivityStrategyFactory.DynamicContext, TrialBalanceEntity> get(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws Exception {
-        return null;
+        return endNode;
     }
 }
