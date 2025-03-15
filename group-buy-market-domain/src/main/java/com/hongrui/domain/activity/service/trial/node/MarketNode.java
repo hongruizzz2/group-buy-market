ee05ed9c2ae8f9b2825cfd2c407d2497abc96e41
hongrui <1169948065@qq.com>
2025-03-15 08:56:51 +0800

    fix: 多线程异步数据加载 - 补充

--- group-buy-market-domain/src/main/java/com/hongrui/domain/activity/service/trial/node/MarketNode.java
+++ group-buy-market-domain/src/main/java/com/hongrui/domain/activity/service/trial/node/MarketNode.java
@@ -32,31 +32,46 @@ public class MarketNode extends AbstractGroupBuyMarketSupport<MarketProductEntit
     private EndNode endNode;
 
     @Override
-    protected void multiThread(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws InterruptedException, TimeoutException, ExecutionException {
+    protected void multiThread(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext
+            dynamicContext) throws InterruptedException, TimeoutException, ExecutionException {
         // 异步查询活动配置
-        QueryGroupBuyActivityDiscountVOThreadTask queryGroupBuyActivityDiscountVOThreadTask = new QueryGroupBuyActivityDiscountVOThreadTask(
+        QueryGroupBuyActivityDiscountVOThreadTask queryGroupBuyActivityDiscountVOThreadTask =
+                new QueryGroupBuyActivityDiscountVOThreadTask(
                     requestParameter.getSource(),
                     requestParameter.getChannel(),
                     repository
                 );
-        FutureTask<GroupBuyActivityDiscountVO> groupBuyActivityDiscountVOFutureTask = new FutureTask<>(queryGroupBuyActivityDiscountVOThreadTask);
+        FutureTask<GroupBuyActivityDiscountVO> groupBuyActivityDiscountVOFutureTask =
+                new FutureTask<>(queryGroupBuyActivityDiscountVOThreadTask);
         threadPoolExecutor.execute(groupBuyActivityDiscountVOFutureTask);
 
         // 异步查询商品信息 - 在实际生产中，有同步库或商品查询接口 这里暂时使用DB方式查询
-        QuerySkuVOFromDBThreadTask querySkuVOFromDBThreadTask = new QuerySkuVOFromDBThreadTask(requestParameter.getGoodsId(), repository);
+        QuerySkuVOFromDBThreadTask querySkuVOFromDBThreadTask = new QuerySkuVOFromDBThreadTask(
+                requestParameter.getGoodsId(),
+                repository
+        );
         FutureTask<SkuVO> skuVOFutureTask = new FutureTask<>(querySkuVOFromDBThreadTask);
         threadPoolExecutor.execute(skuVOFutureTask);
 
         // 写入上下文
-        dynamicContext.setGroupBuyActivityDiscountVO(groupBuyActivityDiscountVOFutureTask.get(timeout, TimeUnit.MINUTES));
-        dynamicContext.setSkuVO(skuVOFutureTask.get(timeout, TimeUnit.MINUTES));
+        dynamicContext.setGroupBuyActivityDiscountVO(groupBuyActivityDiscountVOFutureTask.get(
+                timeout,
+                TimeUnit.MINUTES)
+        );
+        dynamicContext.setSkuVO(skuVOFutureTask.get(
+                timeout,
+                TimeUnit.MINUTES)
+        );
 
-        log.info("拼团商品查询试算服务-MarketNode userId:{} 异步线程加载数据「GroupBuyActivityDiscountVO、SkuVO」完成", requestParameter.getUserId());
+        log.info("拼团商品查询试算服务-MarketNode userId:{} 异步线程加载数据「GroupBuyActivityDiscountVO、SkuVO」完成",
+                requestParameter.getUserId());
     }
 
     @Override
-    protected TrialBalanceEntity doApply(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws Exception {
-        log.info("拼团商品查询试算服务-MarketNode userId:{} requestParameter:{}", requestParameter.getUserId(), JSON.toJSONString(requestParameter));
+    protected TrialBalanceEntity doApply(MarketProductEntity requestParameter,
+                                         DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws Exception {
+        log.info("拼团商品查询试算服务-MarketNode userId:{} requestParameter:{}", requestParameter.getUserId(),
+                JSON.toJSONString(requestParameter));
 
         // todo xfg 拼团优惠试算
 
@@ -65,7 +80,8 @@ public class MarketNode extends AbstractGroupBuyMarketSupport<MarketProductEntit
     }
 
     @Override
-    public StrategyHandler<MarketProductEntity, DefaultActivityStrategyFactory.DynamicContext, TrialBalanceEntity> get(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws Exception {
+    public StrategyHandler<MarketProductEntity, DefaultActivityStrategyFactory.DynamicContext,
+            TrialBalanceEntity> get(MarketProductEntity requestParameter, DefaultActivityStrategyFactory.DynamicContext dynamicContext) throws Exception {
         return endNode;
     }
 }
